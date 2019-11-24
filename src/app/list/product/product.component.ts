6ff0700e733c5730fb0b7389c792f501c89529dc
NhuTien2721996 <nhutien2721996>
2019-11-24 21:28:18 +0700

    create service and delete

--- src/app/list/product/product.component.ts
+++ src/app/list/product/product.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {IProduct} from '../../../IProduct';
+import {ProductServiceService} from '../../product-service.service';
 
 @Component({
   selector: 'app-product',
@@ -7,78 +8,15 @@ import {IProduct} from '../../../IProduct';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  listProduct = this.productService.getAll();
 
-  constructor() {
+  constructor(private productService: ProductServiceService) {
   }
 
-  public products: IProduct[] = [
-    {
-      id: 1,
-      name: 'iphone 7',
-      price: '9.300.000đ',
-      image: 'assets/ip7.png'
-    },
-    {
-      id: 2,
-      name: 'iphone XsMax',
-      price: '38.000.000đ',
-      image: 'assets/xsMax.png'
-    },
-    {
-      id: 3,
-      name: 'iphone 8',
-      price: '14.600.000đ',
-      image: 'assets/8.png'
-    },
-    {
-      id: 4,
-      name: 'Samsung Galaxy Note 10 Plus',
-      price: '16,490,000đ',
-      image: 'assets/thumb_Note10_plus-1.jpg'
-    },
-    {
-      id: 5,
-      name: 'Samsung Galaxy A80',
-      price: '11,990,000đ',
-      image: 'assets/thumb_A80-1.jpg'
-    },
-    {
-      id: 6,
-      name: 'Xiaomi Mi CC9 Pro ',
-      price: '10,490,000đ',
-      image: 'assets/thumb_CC9pro-1.jpg'
-    },
-  ];
-  widthImg = 100;
-  showImage = true;
-  productFilter: IProduct[] = [];
-
-  showHide() {
-    this.showImage = !this.showImage;
-  }
-
-  filter(event) {
-    const keyword = event.target.value;
-    console.log(keyword);
-    this.productFilter = (keyword) ? this.search(keyword) : this.products;
-  }
-
-  search(keyword) {
-    return this.products.filter(
-      product => product.name.toLowerCase().indexOf(keyword) !== -1
-    );
-  }
-
-  delete(id: number) {
-    const confirmResult = confirm('Bạn có muốn xóa không?');
-    if (confirmResult) {
-      this.products.splice(id, 1);
-    }
-  }
-  onSubmit(){
-
-  }
   ngOnInit() {
-    this.productFilter = this.products;
+  }
+
+  delete(id) {
+    this.productService.delete(id);
   }
 }
